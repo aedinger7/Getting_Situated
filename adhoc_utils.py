a5828828e8ef1fe58874401d8c50391d63b927c5
Andy Edinger <aedinge@iu.edu>
2021-08-18 14:48:00 -0400

    fix import

--- adhoc_utils.py
+++ adhoc_utils.py
@@ -1,3 +1,15 @@
+import pandas as pd
+import math
+from transformers import BertTokenizer, BertForMaskedLM
+from torch.nn import functional as F
+import torch
+from transformers import AutoModel, AutoTokenizer 
+from transformers import RobertaTokenizer, RobertaForMaskedLM
+from transformers import logging
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+mask = tokenizer.mask_token
+
 def flatten_data(data, subdata):
     test = [[(x.lower().strip("(s)"), subdata[i][x]) for x in subdata[i]] for i in data if i in subdata] + [(i.lower().strip("(s)"), data[i]) for i in data if i not in subdata]
     return dict([i for sublist in test for i in sublist if type(sublist) == list] + [i for i in test if type(i) != list])
@@ -49,6 +61,43 @@ def compare_masks(masked_sentences, print_results=True, topk=20, model_names=['b
     return results
 
 
+#Reads and parses dunlosky norms data and returns dictionary
+def dunlosky_norms():
+    raw = pd.read_csv("dunlosky_norms_unparsed.csv")
+
+    parsed={}
+
+    for n, line in raw.iterrows():
+        if isinstance(line[0], str) and len(line[0].split((". "))) > 1:
+            key = line[0].split((". "))[1]
+            parsed[key] = {}
+            print(n, key)
+            k = 1
+            while not (isinstance(raw.iloc[n+k, 0], str) and len(raw.iloc[n+k, 0].split((". "))) > 1) and n+k<len(raw)-1:
+                if raw.iloc[n+k, 0] == 'Response' or isinstance(raw.iloc[n+k, 0], float):
+                    print('null row')
+                else:
+                    if raw.iloc[n+k, 0][0] != '\xa0':
+                        subkey = raw.iloc[n+k, 0]
+                        parsed[key][subkey] = raw.iloc[n+k][1:].to_dict()
+                        parsed[key][subkey]['variations'] = [raw.iloc[n+k, 0]]
+                    else:
+                        parsed[key][subkey]['variations'].append(raw.iloc[n+k, 0].replace('\xa0', ''))
+                k+=1
+
+    for cue in parsed:
+        for response in parsed[cue]:
+            variations = []
+            print(parsed[cue][response]['variations'])
+            for word in parsed[cue][response]['variations']:
+                variations += [word.replace("(s)", ""), word.replace("(s)", "s")]
+            parsed[cue][response]['variations'] += variations
+            parsed[cue][response]['variations'] = list(set(parsed[cue][response]['variations']))
+            print(parsed[cue][response]['variations'])
+
+    return parsed
+
+#Returns  associated masked phrases for dunlosky norms data
 def dunlosky_masks():
     return ['a <MASK> is a precious stone',
     'a <MASK> is a unit of time',
@@ -108,6 +157,7 @@ def dunlosky_masks():
     'a <MASK> is a thing women wear']
 
 
+# Returns token scores for sentence with cases based on mask phrase formats
 def get_token_scores(sentence, model='BERT', topk=100):
     if model=='BERT':
         if "a <MASK>" in sentence:
@@ -124,7 +174,7 @@ def get_token_scores(sentence, model='BERT', topk=100):
     if model=='w2v':
         return w2v_getn(sentence)
     
-
+# Compares list of token scores from model responses with norms data
 def correct_responses(token_scores, category):
     responses = parsed[category]
     correct = []
@@ -136,5 +186,57 @@ def correct_responses(token_scores, category):
             missing.append(response)
     return correct, missing, len(correct)/len(responses)
 
+from itertools import chain, groupby
+from operator import itemgetter
+from collections import Counter
 
+# Get max of each item from 2 dictionaries
+def dict_max(data1, data2):
+    get_key, get_val = itemgetter(0), itemgetter(1)
+    merged_data = sorted(chain(data1.items(), data2.items()), key=get_key)
+    merged = {k: max(map(get_val, g)) for k, g in groupby(merged_data, key=get_key)}
+    norm = sum(merged.values())
+    return {key:merged[key]/norm for key in merged.keys()}
+
+# Get mean of items in 3 dictionaries, with weight=0 if item not in dict
+def dict_mean(A,B,C={}):
+    sums = dict(Counter(A) + Counter(B) + Counter(C))
+#     print(sums)
+    div=3
+    if C=={}: # 2 dict case
+        div = 2
+    return {k:sums[k]/div for k in sums}
+
+import re
+
+# Preprocesses masked sentence into tokens list for w2v similarity analysis
+# Helper function for w2v_getn
+def w2v_pre(sentence):
+    tokens = re.split('-| ', sentence.lower())
+    tokens = [x.strip("():,.?").replace("\'", "") for x in tokens]
+    for item in ['<mask>', 'youve', '', 'theyll']:
+        if item in tokens:
+            tokens.remove(item)
+            
+    tokens = [x for x in tokens if nlp(x)[0].pos_ in ['NOUN', 'VERB', 'ADJ']]
+            
+    return tokens
+
+# Get topn similar tokens to sentence
+def w2v_getn(sentence, topn=100, model=glove_vectors):
+    tokens = w2v_pre(sentence)
+    print("w2v tokens:", tokens)
+    length = 0
+    while length < 100:
+        out = {token:score for (token, score) in model.most_similar(tokens, topn=topn) if nlp(token)[0].pos_ in ['NOUN', 'VERB']}
+        length = len(out)
+        topn += 10
+    
+    if len(out) > 100:
+        out = dict(sorted(out.items(), key=lambda x: x[1], reverse=True)[:100])
+        
+    return out
+
+def intersect(s1, s2):
+    return [x for x in s1 if x in s2]
     
